977132ecdcd11f3286191010f5ae0b56f19ef18c
elpilts <elpiniki.litso@best-eu.org>
2024-03-30 00:42:40 +0200

    message

--- script.js
+++ script.js
@@ -26,27 +26,24 @@ function rotateAboutImage() {
 setInterval(rotateAboutImage, 3000);
 
 // Rotate images in the .events section
-var eventsImage = document.querySelector(".events-image");
+
 var eventsCurrentPos = 0;
 var eventsImages = ["Event1.jpg", "Event2.jpg", "Event3.jpg"];
+var eventsTitles = ["EBEC", "BEST Course in Summer", "JobFair"]; // Changed the variable name to eventsTitles
+var eventsTexts = ["About Text 1", "About Text 2", "About Text 3"];
+var eventsImage = document.querySelector(".events-image");
+var eventsText = document.querySelector(".events-text");
+var eventsTitle = document.querySelector(".events-title");
+
 function rotateEventsImage() {
-  if (++eventsCurrentPos >= eventsImages.length)
-    eventsCurrentPos = 0;
+    // Increment the current position and loop back to the start if necessary
+    eventsCurrentPos = (eventsCurrentPos + 1) % eventsImages.length;
     eventsImage.src = eventsImages[eventsCurrentPos];
+    eventsText.textContent = eventsTexts[eventsCurrentPos];
+    eventsTitle.textContent = eventsTitles[eventsCurrentPos]; // Corrected variable name
 }
+
+// Call rotateEventsImage function every 3000 milliseconds (3 seconds)
 setInterval(rotateEventsImage, 3000);
 
-/*var aboutImages = ["Picture1.jpg", "Picture2.jpg", "Picture3.jpg"];
-var aboutTexts = ["About Text 1", "About Text 2", "About Text 3"];
-var aboutImage = document.querySelector(".about-image");
-var aboutText = document.querySelector(".about-text");
-var currentPos = 0;
 
-function rotateImage() {
-    if (++currentPos >= aboutImages.length)
-        currentPos = 0;
-    aboutImage.src = aboutImages[currentPos];
-    aboutText.textContent = aboutTexts[currentPos]; // Update text
-}
-
-setInterval(rotateImage, 3000); */
